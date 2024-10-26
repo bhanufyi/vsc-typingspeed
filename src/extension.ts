1b347b3156a5d03a867c3fbffd5b039fd09ad812
bhanufyi <bhanuprasadcherukuvada2000@gmail.com>
2024-10-26 15:41:03 -0400

    feat: show cpm, wpm 🐋

--- src/extension.ts
+++ src/extension.ts
@@ -1,73 +1,85 @@
-'use strict';
-import { Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, window } from 'vscode';
-import RollingArray from './rollingqueue';
+"use strict";
+import {
+  Disposable,
+  ExtensionContext,
+  StatusBarAlignment,
+  StatusBarItem,
+  window,
+  workspace,
+} from "vscode";
+import RollingArray from "./rollingqueue";
 
 export function activate(context: ExtensionContext) {
-    let typingSpeed = new TypingSpeed();
-    let controller = new TypingSpeedController(typingSpeed);
+  let typingSpeed = new TypingSpeed();
+  let controller = new TypingSpeedController(typingSpeed);
 
-    context.subscriptions.push(controller);
-    context.subscriptions.push(typingSpeed);
+  context.subscriptions.push(controller);
+  context.subscriptions.push(typingSpeed);
 }
 
 class TypingSpeed {
-    private _keystrokes: RollingArray = new RollingArray(100);
-    private _statusBarItem: StatusBarItem;
-    private _lastUpdate: number = 0;
+  private _keystrokes: RollingArray = new RollingArray(100);
+  private _statusBarItem: StatusBarItem;
+  private _lastUpdate: number = 0;
 
-    public updateSpeed() {
-
-        if (!this._statusBarItem) {
-            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
-        }
-
-        const now = Date.now();
-        if (now - this._lastUpdate > 2000) {
-            this._keystrokes.clear();
-        }
-        this._lastUpdate = now;
-        this._keystrokes.add(now);
-
-        let cpm = 0;
-        const count = this._keystrokes.size();
-        if (count > 1) {
-            const firstTime = this._keystrokes.first();
-            cpm = count * 60000 / (now - firstTime);
-        }
-
-        this._statusBarItem.text = `$(keyboard) ${cpm.toFixed(1)} cpm`;
-        this._statusBarItem.show();
+  public updateSpeed() {
+    if (!this._statusBarItem) {
+      this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
     }
 
-    dispose() {
-        this._statusBarItem.dispose();
+    const now = Date.now();
+    if (now - this._lastUpdate > 2000) {
+      this._keystrokes.clear();
     }
+    this._lastUpdate = now;
+    this._keystrokes.add(now);
+    // Read the configuration option
+    const config = workspace.getConfiguration("typingSpeed");
+    const displayType = config.get<string>("display", "wpm");
+
+    let cpm = 0;
+    let wpm = 0;
+    const count = this._keystrokes.size();
+    if (count > 1) {
+      const firstTime = this._keystrokes.first();
+      cpm = (count * 60000) / (now - firstTime);
+      wpm = cpm / 5;
+    }
+
+    this._statusBarItem.text = `$(keyboard) ${
+      displayType === "cpm" ? `${cpm.toFixed(1)} cpm` : `${wpm.toFixed(1)} wpm`
+    }`;
+    this._statusBarItem.show();
+  }
+
+  dispose() {
+    this._statusBarItem.dispose();
+  }
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
 
 class TypingSpeedController {
-    private _typingSpeed: TypingSpeed;
-    private _disposable: Disposable;
+  private _typingSpeed: TypingSpeed;
+  private _disposable: Disposable;
 
-    constructor(typingSpeed: TypingSpeed) {
-        this._typingSpeed = typingSpeed;
+  constructor(typingSpeed: TypingSpeed) {
+    this._typingSpeed = typingSpeed;
 
-        let subscriptions: Disposable[] = [];
-        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+    let subscriptions: Disposable[] = [];
+    window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
 
-        this._typingSpeed.updateSpeed();
+    this._typingSpeed.updateSpeed();
 
-        this._disposable = Disposable.from(...subscriptions);
-    }
+    this._disposable = Disposable.from(...subscriptions);
+  }
 
-    dispose() {
-        this._disposable.dispose();
-    }
+  dispose() {
+    this._disposable.dispose();
+  }
 
-    private _onEvent() {
-        this._typingSpeed.updateSpeed();
-    }
+  private _onEvent() {
+    this._typingSpeed.updateSpeed();
+  }
 }
